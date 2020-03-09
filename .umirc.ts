c1e335f7cce67955bb99597359fc548436de6f37
liujinyang <779542115@qq.com>
2020-03-09 23:31:34 +0800

    更新配置文件

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Melon的博客',
   mode: 'site',
+  history: {
+    type: 'hash',
+  },
+  hash: true,
   navs: [null],
   // more config: https://d.umijs.org/config
 });
